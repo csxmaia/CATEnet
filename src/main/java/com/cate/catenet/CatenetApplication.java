66b0309a20bfee73ae9496b2f1c341bb3b52f466
Cristhian <cristhian.smaia@gmail.com>
2021-07-26 20:37:57 -0300

    add api

---
+++ src/main/java/com/cate/catenet/CatenetApplication.java
@@ -0,0 +1,13 @@
+package com.cate.catenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatenetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cate/catenet/CatenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cate.catenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
