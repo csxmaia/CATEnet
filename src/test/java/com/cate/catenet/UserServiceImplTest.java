313c01195540d652e96fbf176380012d55e2adfd
Cristhian <cristhian.smaia@gmail.com>
2021-09-02 21:52:05 -0300

    implementação teste solicitacoes

--- src/test/java/com/cate/catenet/UserServiceImplTest.java
+++ src/test/java/com/cate/catenet/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.cate.catenet;
 
+import com.cate.catenet.enums.StatusUserEnum;
 import com.cate.catenet.exception.ApiResponseException;
 import com.cate.catenet.model.User;
 import com.cate.catenet.service.AuthServiceImpl;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -48,6 +51,12 @@ public class UserServiceImplTest {
         } catch (ApiResponseException e) {
             Assert.assertEquals("Nenhum usuario encontrado", e.getMessage());
         }
+    }
+
+    @Test
+    public void getSolicitations() throws ApiResponseException {
+        List<User> users = userService.getSolicitations();
+        Assert.assertTrue(users.get(0).getStatus().equals(StatusUserEnum.ENTRY_ACCESS.name()));
     }
 
 }
